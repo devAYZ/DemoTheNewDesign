084941d8ce46a45eaab38202d046b14d396b3135
devayz <fatokimiemman@gmail.com>
2025-10-04 09:25:04 +0100

    make successful network call from internal struct

--- DemoTheNewDesign/MV Pattern/ProductListView.swift
+++ DemoTheNewDesign/MV Pattern/ProductListView.swift
@@ -17,15 +17,29 @@ struct Product: Decodable, Identifiable, Equatable {
 
 struct HTTPClient {
     func getProductList() async throws -> [Product] {
-        let (data, _) = try await URLSession.shared.data(from: .init(string: "[GET] https://api.escuelajs.co/api/v1/products")!)
-        
-        return try JSONDecoder().decode([Product].self, from: data)
+        let (data, _) = try await URLSession.shared
+            .data(from: .init(string: "https://api.escuelajs.co/api/v1/products")!)
+        return try JSONDecoder()
+            .decode([Product].self, from: data)
     }
 }
 
 struct ProductListView: View {
+    @State var productData: [Product]?
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        }
+        .task {
+            do {
+                let data = try await HTTPClient().getProductList()
+                print(data.first?.title ?? "-")
+                productData = data
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
     }
 }
 
