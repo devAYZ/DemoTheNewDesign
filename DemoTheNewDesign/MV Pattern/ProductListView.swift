118b2667a85b70518d664537778e1cda6ba1c3cf
devayz <fatokimiemman@gmail.com>
2025-10-04 08:31:51 +0100

    add a http client

--- DemoTheNewDesign/MV Pattern/ProductListView.swift
+++ DemoTheNewDesign/MV Pattern/ProductListView.swift
@@ -7,6 +7,22 @@
 
 import SwiftUI
 
+struct Product: Decodable, Identifiable, Equatable {
+    var id: Int?
+    var title: String?
+    var slug: String?
+    var price: Double?
+    var description: String?
+}
+
+struct HTTPClient {
+    func getProductList() async throws -> [Product] {
+        let (data, _) = try await URLSession.shared.data(from: .init(string: "[GET] https://api.escuelajs.co/api/v1/products")!)
+        
+        return try JSONDecoder().decode([Product].self, from: data)
+    }
+}
+
 struct ProductListView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
